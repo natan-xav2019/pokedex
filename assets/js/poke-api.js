65a0a8064fc946430797f8d909e6fd8f3c6bde9c
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2024-11-29 19:23:00 -0300

    Feat: PokemonToLi mais dinamico

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,16 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then(response => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then(response => response.json())
         .then(jsonBody => jsonBody.results)
+        .then(pokemons => pokemons.map(pokeApi.getPokemonDetail))
+        .then(detalRequests => Promise.all(detalRequests))
         .catch(error => console.error(error))
 }

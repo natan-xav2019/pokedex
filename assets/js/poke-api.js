8a27ce10ba13b51868a727e1b704408f5b0e5018
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2024-12-02 10:08:44 -0300

    Fix: Correção numeração pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,7 +5,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
     pokemon.types = types
     pokemon.type = type
